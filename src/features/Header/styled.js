76a8561c83bbbbf7847fcfd40d88d9e4eb4310d6
Wolian <krzys_banasiak@o2.pl>
2022-09-30 16:59:58 +0200

    heders style prepared

---
+++ src/features/Header/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+`;
+
+export const Foto = styled.div`
+  
+`;
+
+export const Image = styled.img`
+
+`;
+
+export const Content = styled.div`
+
+`;
+
+export const ThisIs = styled.p`
+  
+`;
+
+export const Name = styled.h1`
+
+`;
+
+export const Description = styled.p`
+  
+`;
+
+export const Email = styled.a`
+
+`;
