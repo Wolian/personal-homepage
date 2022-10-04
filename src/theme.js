b969145c87831cabeeb24db97ec3aa1ab583c0d6
Wolian <krzys_banasiak@o2.pl>
2022-10-04 14:56:24 +0200

    theme colors added

--- src/theme.js
+++ src/theme.js
@@ -3,5 +3,10 @@ export const theme = {
         whiteLilac: "#FBFBFE", //background
         black: "#252525", //textColor
         slateGray: "#6E7E91", //textColorLight
+        scienceBlue: "#0366D6", //buttonBackground
+        iron: "#D1D5DA", //buttonThemeBackground
+        white: "#FFFFFF", //buttonText
+        blue: "#2517EB", //buttonFocus
+        anakiwa: "#8CC2FF", //buttonFocusShadow
     }
 };
