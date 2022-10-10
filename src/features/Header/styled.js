b6db57fcbbd654d2091f80f26c2ada4b9f25531f
Wolian <krzys_banasiak@o2.pl>
2022-10-10 15:55:17 +0200

    mobile styling added

--- src/features/Header/styled.js
+++ src/features/Header/styled.js
@@ -6,35 +6,63 @@ export const Wrapper = styled.header`
   grid-gap: 66px;
   transition: 0.4s;
   max-width: 1089px;
+  
+  @media (max-width: 767px){
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+  };
 `;
 
 export const Image = styled.img`
-  width: 398px;
-  height: 398px;
+  max-width: 398px;
+  max-height: 398px;
   border-radius: 50%;
+  
+  @media (max-width: 767px){
+    max-width: 133px;
+    max-height: 133px;
+    margin-top: 12px;
+    
+  }
 `;
 
 export const Content = styled.div`
-  
+  margin-top: 64px;
+
+  @media (max-width: 767px){
+    margin-top: 12px;
+  };
 `;
 
 export const ThisIs = styled.p`
   font-weight: 700;
   font-size: 12px;
-  margin-bottom: 12px;
   color: ${({ theme }) => theme.colors.slateGray};
+
+  @media (max-width: 767px){
+    margin: 0;
+  };
 `;
 
 export const Name = styled.h1`
   font-weight: 900;
   font-size: 38px;
   color: ${({ theme }) => theme.colors.black};
+  
+  @media (max-width: 767px){
+    font-size: 22px;
+  };
 `;
 
 export const Description = styled.p`
   font-weight: 400;
   font-size: 20px;
   color: ${({ theme }) => theme.colors.slateGray};
+
+  @media (max-width: 767px){
+    font-size: 17px;
+  };
 `;
 
 export const Email = styled.a`
