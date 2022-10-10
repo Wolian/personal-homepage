9a99ac9bf3ae43069326d2dabcd13d99e4d023d5
Wolian <krzys_banasiak@o2.pl>
2022-10-10 15:54:59 +0200

    MainTile styled

---
+++ src/common/MainTile/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  margin-top: 63px;
+  padding: 32px;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+`;
+
+export const Header = styled.h2`
+  font-weight: 900;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
+
+export const Content = styled.div`
+  margin-top: 15px;
+  padding-top: 15px;
+  border-top: 1px solid rgba(209, 213, 218, 0.3);
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.slateGray};
+`;
