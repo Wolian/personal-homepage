1fdce9f256e47a6261e9475c5a7e5a5516c71901
Wolian <krzys_banasiak@o2.pl>
2022-10-13 15:12:56 +0200

    Loading component finished

--- src/common/Loading/index.js
+++ src/common/Loading/index.js
@@ -1,7 +1,8 @@
-import {Title, Wrapper} from "./styled";
+import {Loader, Title, Wrapper} from "./styled";
 
 export const Loading = () => (
     <Wrapper>
         <Title>Please wait, projects are being loaded...</Title>
+        <Loader/>
     </Wrapper>
 );
--- src/common/Loading/styled.js
+++ src/common/Loading/styled.js
@@ -10,6 +10,22 @@ export const Title = styled.p`
   margin-bottom: 48px;
 `;
 
-export const Loading = styled.div`
-
+export const Loader = styled.div`
+  width: 160px;
+  height: 160px;
+  border: 11.375px solid rgba(209, 213, 218, 0.3);
+  border-left-color: ${({ theme }) => theme.colors.scienceBlue};
+  margin: 0 auto;
+  border-radius: 50%;
+  animation: spinLoad 0.7s infinite linear;
+  
+  @keyframes spinLoad {
+    0% {
+      transform: rotate(0deg);
+    }
+    
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
