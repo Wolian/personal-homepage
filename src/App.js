f1dec1c86aff20b6537370d9a3d1cea097e487e6
Wolian <krzys_banasiak@o2.pl>
2022-10-13 15:13:26 +0200

    small fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {Header} from "./features/Header";
 import {Skills} from "./features/Skills";
 import {NextLearn} from "./features/NextLearn";
 import {Portfolio} from "./features/Portfolio";
-import {Loading} from "./common/Loading";
 
 export const App = () => {
     return(
@@ -11,7 +10,6 @@ export const App = () => {
             <Skills/>
             <NextLearn/>
             <Portfolio/>
-            <Loading/>
         </>
     );
 };
