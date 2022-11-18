02a241ac6ae5765b25d4099ed96046a013af63f2
Wolian <krzys_banasiak@o2.pl>
2022-11-18 14:03:22 +0100

    saga updated

--- src/core/store.js
+++ src/core/store.js
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import personalHomepageReducer from "../features/personallHomepage/personalHomepageSlice";
+import {personalHomepageSaga} from "../features/personallHomepage/personalHomepageSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         personalHomepage: personalHomepageReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(personalHomepageSaga);
+
+export default store;
 
 
 
