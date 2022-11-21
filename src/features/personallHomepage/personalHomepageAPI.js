2f3f33c00c9ec2d33951160c27c3531654e1caef
Wolian <krzys_banasiak@o2.pl>
2022-11-21 12:34:29 +0100

    api link updated

--- src/features/personallHomepage/personalHomepageAPI.js
+++ src/features/personallHomepage/personalHomepageAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const getRepositories = () =>
-    axios.get("https://docs.github.com/users/Wolian/repos")
+    axios.get("https://api.github.com/users/Wolian/repos")
         .then(response => response.data);
