11cebffd5f7005c93f02ee4a6ff08e6b00712fa1
Wolian <krzys_banasiak@o2.pl>
2022-11-21 15:19:16 +0100

    projectTile fixed

--- src/common/ProjectTile/index.js
+++ src/common/ProjectTile/index.js
@@ -1,18 +1,18 @@
 import {Description, Link, PreLink, Repo, Tile, Title} from "./styled";
 
-export const ProjectTile = ({ gitRepo }) => {
-    return gitRepo?.map((repo) => (
-        <Tile key={repo.id}>
-            <Title>{repo.name}</Title>
-            <Description>{repo.description}</Description>
-            <Repo>
-                <PreLink>Demo:</PreLink>
-                <Link href={repo.homepage}>{repo.homepage}</Link>
-            </Repo>
-            <Repo>
-                <PreLink>Code:</PreLink>
-                <Link href={repo.html_url}>{repo.html_url}</Link>
-            </Repo>
-        </Tile>
+export const ProjectTile = ({ repositories }) => (
+    repositories?.map((repo) => (
+            <Tile key={repo.id}>
+                <Title>{repo.name}</Title>
+                <Description>{repo.description}</Description>
+                <Repo>
+                    <PreLink>Demo:</PreLink>
+                    <Link href={repo.homepage}>{repo.homepage}</Link>
+                </Repo>
+                <Repo>
+                    <PreLink>Code:</PreLink>
+                    <Link href={repo.html_url}>{repo.html_url}</Link>
+                </Repo>
+            </Tile>
+        )
     ));
-};
