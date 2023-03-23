d0c1d7b33cf7308197f2a47f44621f84c4cffb47
Wolian <krzys_banasiak@o2.pl>
2023-03-23 15:02:06 +0100

    non-breaking space added to loading

--- src/features/personallHomepage/Portfolio/ProjectTile/Loading/index.js
+++ src/features/personallHomepage/Portfolio/ProjectTile/Loading/index.js
@@ -2,7 +2,7 @@ import {Loader, Title, Wrapper} from "./styled";
 
 export const Loading = () => (
     <Wrapper>
-        <Title>Please wait, projects are being loaded...</Title>
+        <Title>Please wait, projects are&nbsp;being&nbsp;loaded...</Title>
         <Loader/>
     </Wrapper>
 );
