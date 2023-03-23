c489ea1a5c1b61b42fad10fd2ef371ae3fd5f2ec
Wolian <krzys_banasiak@o2.pl>
2023-03-23 15:24:02 +0100

    Tiles fixed

--- src/features/personallHomepage/Portfolio/ProjectTile/Repositories/index.js
+++ src/features/personallHomepage/Portfolio/ProjectTile/Repositories/index.js
@@ -1,18 +1,20 @@
-import {Description, Link, PreLink, Repo, Tile, Title} from "./styled";
+import {Description, Link, List, PreLink, Repo, Tile, Title} from "./styled";
 
 export const Repositories = ({ repositories }) => (
-    repositories?.map((repo) => (
-            <Tile key={repo.id}>
-                <Title>{repo.name}</Title>
-                <Description>{repo.description}</Description>
-                <Repo>
-                    <PreLink>Demo:</PreLink>
-                    <Link href={repo.homepage}>{repo.homepage}</Link>
-                </Repo>
-                <Repo>
-                    <PreLink>Code:</PreLink>
-                    <Link href={repo.html_url}>{repo.html_url}</Link>
-                </Repo>
-            </Tile>
-        )
-    ));
+     <List>
+         {repositories?.map((repo) => (
+        <Tile key={repo.id}>
+            <Title>{repo.name}</Title>
+            <Description>{repo.description}</Description>
+            <Repo>
+                <PreLink>Demo:</PreLink>
+                <Link href={repo.homepage}>{repo.homepage}</Link>
+            </Repo>
+            <Repo>
+                <PreLink>Code:</PreLink>
+                <Link href={repo.html_url}>{repo.html_url}</Link>
+            </Repo>
+        </Tile>
+        ))}
+    </List>
+);
