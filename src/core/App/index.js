1e362e270ebc4ff677a5788e48ffdccc835edcc3
Wolian <krzys_banasiak@o2.pl>
2023-04-12 16:16:25 +0200

    reorganized App.js

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,19 +1,31 @@
-import {Header} from "../../features/personallHomepage/Header";
-import {Skills} from "../../features/personallHomepage/Skills";
-import {NextLearn} from "../../features/personallHomepage/NextLearn";
-import {Portfolio} from "../../features/personallHomepage/Portfolio";
-import {Footer} from "../../features/personallHomepage/Footer";
+import { Header } from "../../features/personallHomepage/Header";
+import { Skills } from "../../features/personallHomepage/Skills";
+import { NextLearn } from "../../features/personallHomepage/NextLearn";
+import { Portfolio } from "../../features/personallHomepage/Portfolio";
+import { Footer } from "../../features/personallHomepage/Footer";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../common/themeSlice";
+import { ThemeProvider } from "styled-components";
+import { ThemeSwitch } from "../../common/ThemeSwitch";
+import { themeDark, themeLight } from "./theme";
+import { Normalize } from "styled-normalize";
+import { GlobalStyle } from "./GlobalStyle";
 
 export const App = () => {
-    return(
-        <>
-            <Header/>
-            <main>
-                <Skills/>
-                <NextLearn/>
-                <Portfolio/>
-            </main>
-            <Footer/>
-        </>
-    );
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <Normalize />
+      <GlobalStyle />
+      <ThemeSwitch />
+      <Header />
+      <main>
+        <Skills />
+        <NextLearn />
+        <Portfolio />
+      </main>
+      <Footer />
+    </ThemeProvider>
+  );
 };
